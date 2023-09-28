4196f65b912c9a1537442ac74d5aa4743447b009
Naomi <47122756+nriveram@users.noreply.github.com>
2023-09-28 01:20:19 -0700

    adding code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,83 @@
-console.log('Test');
+/**
+ * The "Name" field 
+ */
+let nameInput = document.getElementById('name')
+nameInput.focus(); 
+
+/**
+ * The "Job Role" section
+ */
+let jobRoleMenu = document.getElementById('title');
+let otherJobInput = document.getElementById('other-job-role');
+otherJobInput.hidden = true; 
+
+jobRoleMenu.addEventListener('change', (e) => {
+    let titleSelected = e.target.value; 
+    if (titleSelected === 'other') {
+        otherJobInput.hidden = false; 
+    } else {
+        otherJobInput.hidden = true;
+    }
+}); 
+
+/**
+ * The "T-Shirt Info" section
+ */
+
+let designsMenu = document.getElementById('design'); 
+let colorsMenu = document.getElementById('color'); 
+let colorsMenuChildren = colorsMenu.children; 
+colorsMenu.disabled = true; 
+
+designsMenu.addEventListener('change', (e) => {
+    colorsMenu.disabled = false;
+    for (let i = 1; i < colorsMenuChildren.length; i++) {
+        let designSelected = e.target.value; 
+        let currentThemeElement = colorsMenuChildren[i]
+        let currentThemeName = currentThemeElement.getAttribute('data-theme'); 
+        
+        if (currentThemeName === designSelected) {
+            currentThemeElement.hidden = false; 
+            currentThemeElement.setAttribute('selected', true); 
+        } else {
+            currentThemeElement.hidden = true; 
+            currentThemeElement.setAttribute('selected', false);
+        }
+    } 
+    /*
+    if (colorsMenu.value !== '') {
+        colorsMenu.firstElementChild.selected = true; 
+    } else {
+        colorsMenu.firstElementChild.selected = false;
+    }
+    */
+}); 
+
+/**
+ * The "Activities" section 
+ */
+
+let activitiesFieldElement = document.getElementById('activities'); 
+let totalCostParagraphElement = document.getElementById('activities-cost'); 
+let totalCost = 0; 
+
+activitiesFieldElement.addEventListener('change', (e) => {
+    let activitySelected = e.target; 
+    let activitySelectedCost = +activitySelected.getAttribute('data-cost'); 
+    if (activitySelected.checked) {
+        totalCost += activitySelectedCost; 
+    } else {
+        totalCost -= activitySelectedCost;
+    }
+    totalCostParagraphElement.innerHTML = `Total: $${totalCost}`; 
+}); 
+
+/**
+ * The "Payment Info" section 
+ */
+
+
+
+
+
+ 
